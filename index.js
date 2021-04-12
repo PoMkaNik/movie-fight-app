288ef85a900591750da440ae4c3d3564e54e5964
Roman Dubovik <ip.romandubovik@gmail.com>
2021-04-12 12:44:59 +0300

    autocomplete widget with dropdown added

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ const API_URL = 'http://www.omdbapi.com/';
 const API_KEY = '7d57367f';
 const DELAY_SEC = 1;
 
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+  <label><b>Search For a Movie</b></label>
+  <input type="text" name="search-1" id="search-1" class="input" />
+  <div class="dropdown">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results">
+      </div>
+    </div>
+  </div>
+`;
+const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
+
 const fetchData = async (query) => {
   const response = await axios.get(API_URL, {
     params: {
       apikey: API_KEY,
       s: query,
-      // i: 'tt0848228',
     },
   });
 
@@ -18,23 +32,58 @@ const fetchData = async (query) => {
   return response.data.Search;
 };
 
-const input = document.querySelector('input');
+const onMovieSelect = async (movie) => {
+  // request additional movie data based on imdb id
+  const response = await axios.get(API_URL, {
+    params: {
+      apikey: API_KEY,
+      i: movie.imdbID,
+    },
+  });
+  //
+  console.log(response);
+};
 
 const onInput = debounce(async (e) => {
   // get query from input field on change
   const searchQuery = e.target.value;
   // get movies from API
   const movies = await fetchData(searchQuery);
+  // if no results
+  if (!movies.length) {
+    // need to 'close' dropdown if we clear the input after successful search
+    dropdown.classList.remove('is-active');
+    return;
+  }
+  // clear resultsWrapper
+  resultsWrapper.innerHTML = '';
+  // open dropdown
+  dropdown.classList.add('is-active');
   // render movies
   for (let movie of movies) {
-    const div = document.createElement('div');
-
-    div.innerHTML = `
-      <img src="${movie.Poster}" />
-      <h1>${movie.Title}</h1>
+    // Bulma is required <a> as item dropdown item wrapper
+    const option = document.createElement('a');
+    // check if there is no img src for poster
+    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+    // add required by Bulma css class
+    option.classList.add('dropdown-item');
+    // create content of new element
+    option.innerHTML = `
+      <img src="${imgSrc}" />
+      ${movie.Title} (${movie.Year})
     `;
+    // listen click on this item
+    option.addEventListener('click', () => {
+      // update input field value
+      // we can reference the movie item in this loop as closure
+      input.value = movie.Title;
+      // close dropdown
+      dropdown.classList.remove('is-active');
+      // get more data for clicked movie
+      onMovieSelect(movie);
+    });
 
-    document.querySelector('#target').appendChild(div);
+    resultsWrapper.appendChild(option);
   }
 }); // =>
 // returns new function
@@ -57,3 +106,7 @@ input.addEventListener('input', onInput); // =>
 // }
 // , DELAY_SEC * 1000);
 // };
+
+document.addEventListener('click', (e) => {
+  if (!root.contains(e.target)) dropdown.classList.remove('is-active');
+});
